0dcb60d358e494a2cdf1aafc6a3452f60edde2b6
nebulaBdj <dongdung5070@gmail.com>
2024-10-27 05:31:00 +0900

    fix: useSearchParams가 서버에서 동작하지 않도록 Suspense로 감쌈

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -1,10 +1,10 @@
 'use client'
 
-import { useEffect } from 'react'
+import { Suspense, useEffect } from 'react'
 import { useSearchParams, useRouter } from 'next/navigation'
 import { SendData } from './type'
 
-export default function LoginCheck() {
+function LoginCheck() {
   const searchParams = useSearchParams()
   const router = useRouter()
 
@@ -76,22 +76,10 @@ export default function LoginCheck() {
   return <div>로그인 정보를 확인중입니다...</div>
 }
 
-// 카카오
-// http://localhost:3000/auth/callback?
-// code=ZZnujfZHgky0SJzoqbsnPu3y2TKkcfDvDbYNMleQV7tf_vIeY5YOYQAAAAQKKiUQAAABksnID2r7Ewsnpgvovw
-
-// 만들어지는 url : https://cnergy.p-e.kr/v1/oauth/kakao?code=7wnKYOCgbr5DjoVxkYN_Pv-V02_ELWEZ4WCS1teNUrqx_R0C5_ZscAAAAAQKKw0eAAABksn8dLxPBWDH3LuH7A
-
-// 구글
-// http://localhost:3000/auth/callback?
-// code=4%2F0AVG7fiTjjq1uNzs9UKtrmM3_BGeROe5RR0vGz3zYjrC1uw_qaSiOT3h1_FRVY-efhY_8Dw&
-// scope=email+profile+openid+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email&authuser=0&prompt=consent
-
-// 만들어지는 url : https://cnergy.p-e.kr/v1/oauth/google?code=4/0AVG7fiSX5k6hZg5rq1Y0mmuDqWkq2BsO3KP4JI_5eUg53128M219Nk58taZ30Yq2Yk-Elw
-
-// 네이버
-// http://localhost:3000/auth/callback?
-// code=3IgulvPnQhcdllTQZ2&
-// state=7d0uu0g1bct5s0n3em5pitb457
-
-// 만들어지는 url :  https://cnergy.p-e.kr/oauth/login/naver?code=3IgulvPnQhcdllTQZ2&state=3IgulvPnQhcdllTQZ2
+export default function WrappedLoginCheck() {
+  return (
+    <Suspense fallback={<div>로딩 중...</div>}>
+      <LoginCheck />
+    </Suspense>
+  )
+}
