2f05d1910124ba360495a0d2bfaf9f2f490c282a
yeyounging <133792082+yeyounging@users.noreply.github.com>
2024-11-03 23:16:29 +0900

    feat : 빠른시작 등록 api

--- src/app/home/api/queries.ts
+++ src/app/home/api/queries.ts
@@ -1,5 +1,6 @@
-import { useSuspenseQuery } from '@tanstack/react-query'
-import { getQuickList } from './api'
+import { useMutation, useSuspenseQuery } from '@tanstack/react-query'
+import { getQuickList, postQuickStart } from './api'
+import { QuickStart, QuickStartRequest } from './type'
 
 export const useGetQuickList = () =>
   useSuspenseQuery({
@@ -7,3 +8,13 @@ export const useGetQuickList = () =>
     queryFn: () => getQuickList(),
     select: (data) => data.data,
   })
+
+export const usePostQuickStart = () => {
+  return useMutation({
+    mutationKey: ['quick-start'],
+    mutationFn: (data: QuickStartRequest) => postQuickStart(data),
+    onMutate: () => {},
+    onSuccess: () => {},
+    onError: () => {},
+  })
+}
