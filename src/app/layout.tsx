58769024ce338ca4ca8444e5cf1acc94c4716cbe
yeyounging <133792082+yeyounging@users.noreply.github.com>
2024-11-24 03:32:19 +0900

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { GlobalErrorBoundary } from '@/react-utils/ErrorBoundary'
 import { Suspense } from 'react'
 import { QueryProvider } from '@/lib'
 import { cn } from '@/util'
-import { pretendard, wavvepado } from './fonts'
+import { pretendard, wavvepado } from '../../public/fonts'
 
 export const metadata: Metadata = {
   title: '나의 시간조각을 모아, 조각조각',
