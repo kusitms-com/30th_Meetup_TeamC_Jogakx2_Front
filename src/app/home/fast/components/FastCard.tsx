8a8b490ce3d7a19beaa05d7fefe6a0ec0a835d5d
yeyounging <133792082+yeyounging@users.noreply.github.com>
2024-11-26 20:17:39 +0900

    feat: 편집시 기존데이터 전달

--- src/app/home/fast/components/FastCard.tsx
+++ src/app/home/fast/components/FastCard.tsx
@@ -6,13 +6,25 @@ import { useQuickStart } from '../../hook/useQuickStart'
 
 export default function FastCard(quickStart: QuickStart) {
   const { push } = useRouter()
-  const { setQuickStartLocal, goToActivity } = useQuickStart()
+  const { goToActivity } = useQuickStart()
 
   const { name, hour, minute, spareTime, meridiem, type } = quickStart
 
   const handleClickEdit = () => {
-    setQuickStartLocal(quickStart)
-    push('fast/add')
+    const isOnline = type === 'ONLINE' || type === 'ONLINE_AND_OFFLINE'
+    const isOffline = type === 'OFFLINE' || type === 'ONLINE_AND_OFFLINE'
+
+    const query = new URLSearchParams({
+      name,
+      hour: hour.toString(),
+      minute: minute.toString(),
+      spareTime: spareTime.toString(),
+      meridiem,
+      isOnline: isOnline.toString(),
+      isOffline: isOffline.toString(),
+    }).toString()
+
+    push(`/home/fast/add?${query}`)
   }
   return (
     <div className="px-16 py-12 flex justify-between w-full items-center border border-primary_foundation_10 rounded-8">
