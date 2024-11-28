3969016879b22b0fc5f5642d74cce2efcfa0ad47
nebulaBdj <dongdung5070@gmail.com>
2024-11-28 21:11:44 +0900

    fix: QA 반영

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,8 @@ import { GlobalErrorBoundary } from '@/react-utils/ErrorBoundary'
 import { Suspense } from 'react'
 import { QueryProvider } from '@/lib'
 import { cn } from '@/util'
-import { pretendard, wavvepado } from '../../public/fonts'
 import MobileWrapper from '@/components/MobileWrapper/MobileWrapper'
+import { pretendard, wavvepado } from '../../public/fonts'
 
 export const metadata: Metadata = {
   title: '나의 시간조각을 모아, 조각조각',
